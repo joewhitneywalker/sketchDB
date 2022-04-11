cec5f5d9e171fe858656b2b8d9ad12dddaa9b7b6
joewhitneywalker <walker.joe81@gmail.com>
2022-04-10 18:42:05 -0700

    styoing sign up page

--- main_app/forms.py
+++ main_app/forms.py
@@ -7,14 +7,3 @@ class FileForm(forms.ModelForm):
     class Meta: 
         model = File
         fields = ('file_name', 'created_by', 'pdf', 'season', 'category', 'comments', 'preview',)
-'''
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('user_name', 'email', 'password')
-        widgets = {
-            'password' : forms.PasswordInput(),
-        }
-
-'''
